c830ca6a493a80a9d8ea057782a402b247f51aeb
Andrew Trigg <andrewt51@hotmail.com>
2016-01-23 13:32:31 +0000

    Used gulp wrap to put concatenated file into doc.ready event handler

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,17 @@ var rename = require('gulp-rename');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var image = require('gulp-image');
+var wrap = require('gulp-wrap');
+
  
 gulp.task('default',['babelify','minify'],() => {
 });
 
 
 gulp.task('babelify',['image'],()=> {
-  return gulp.src(['src/firstLine','src/animation.js','src/newDeck.js','src/shuffle.js','src/main.js','src/lastLine'])
+  return gulp.src(['src/animation.js','src/newDeck.js','src/shuffle.js','src/main.js'])
     .pipe(concat('main.js'))
+    .pipe(wrap('document.addEventListener("DOMContentLoaded", ()=>{\n<%= contents %>\n});'))
     .pipe(babel({
       presets: ['es2015']
     }))
